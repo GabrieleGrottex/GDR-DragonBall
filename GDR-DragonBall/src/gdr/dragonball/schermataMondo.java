f91c06a167d7c9b91a68d7e809c2dd8f57d65d04
grottelli-gabriele <grottelli.gabriele@franchettisalviani.net>
2026-03-23 09:14:08 +0100

    cambiamento
    inserimento altre immagini per i personaggi

--- GDR-DragonBall/src/gdr/dragonball/schermataMondo.java
+++ GDR-DragonBall/src/gdr/dragonball/schermataMondo.java
@@ -8,14 +8,14 @@ package gdr.dragonball;
  *
  * @author grottelli.gabriele
  */
-public class SceltaMappa extends javax.swing.JFrame {
+public class schermataMondo extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(SceltaMappa.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(schermataMondo.class.getName());
 
     /**
      * Creates new form SceltaMappa
      */
-    public SceltaMappa() {
+    public schermataMondo() {
         initComponents();
     }
 
@@ -87,7 +87,7 @@ public class SceltaMappa extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new SceltaMappa().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new schermataMondo().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
